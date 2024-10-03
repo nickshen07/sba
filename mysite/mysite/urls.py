cff10f355eeaa794c88b2175e8eb0d97033ac38b
nickshen07 <shenlikhang@yahoo.com.hk>
2024-10-04 00:03:43 +0800

    20241004, 0:03

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path('', include('tasks.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
     label=models.CharField(max_length=255, default=None, null = True)
     status=models.BooleanField(default=False, null = True)
     deadline=models.DateTimeField(default=None, null = True)
-
+    image = models.ImageField(upload_to='img')
     def __str__(self):
         return str(self.taskid) + '. ' + self.title
 
