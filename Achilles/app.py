8c6002bbf698fa97512fedbeda669798c172b5b1
nickshen07 <shenlikhang@yahoo.com.hk>
2024-10-11 23:07:41 +0800

    20241011, 23:07

--- Achilles/app.py
+++ Achilles/app.py
@@ -4,12 +4,23 @@ from sqlalchemy import create_engine
 from db import *
 import sqlite3
 import mysql.connector
+from werkzeug.exceptions import HTTPException
+import json
+import requests
 
 app = Flask(__name__)
+
+def get_meme():
+    url = "https://meme-api.com/gimme"
+    res = json.loads(requests.request("GET", url).text)
+    return res['url']
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
+        if 'reset' in request.form:
+            reset()
+            return redirect('/')
         task_content = request.form['content']
         b = 1 if 'status' in request.form else 0
         query = f"INSERT INTO Tasks (Name, Status) VALUES ('{task_content}', {b})"
@@ -20,9 +31,9 @@ def index():
         except:
             return render_template('error.html', s="invalid input")
     else:
-        task = raww("SELECT * FROM Tasks")
-        print(task)
-        return render_template('index.html', tasks = task)
+        uncom = raww("SELECT * FROM Tasks WHERE Status = 0")
+        com = raww("SELECT * FROM Tasks WHERE Status = 1")
+        return render_template('index.html', uncom = uncom, com = com, url= get_meme())
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -49,6 +60,12 @@ def update(id):
             return render_template('error.html', s="Task ID invalid")
         item = item[0]
         return render_template('update.html', item=item)
+
+@app.errorhandler(HTTPException)
+def handleError(err):
+    return render_template('error.html', s=str(err.code)+" "+err.name+" "+err.description)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
