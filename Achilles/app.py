3c5d6fe63e0d15a6e05c56e3dad0c79f6b45f64a
nickshen07 <shenlikhang@yahoo.com.hk>
2024-10-13 19:11:47 +0800

    20241013, 19:11

--- Achilles/app.py
+++ Achilles/app.py
@@ -7,6 +7,8 @@ import mysql.connector
 from werkzeug.exceptions import HTTPException
 import json
 import requests
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
@@ -17,6 +19,8 @@ def get_girl():
 def index():
     if request.method == "POST":
         if 'reset' in request.form:
+            with open("reset.txt", 'w') as f:
+                f.write(datetime.now(pytz.timezone('Asia/Hong_Kong')).strftime("%Y-%m-%d %H:%M:%S"))
             reset()
             return redirect('/')
         print(request.form)
@@ -25,20 +29,28 @@ def index():
         opt = request.form['opt']
         date = request.form['date']
         query = f"INSERT INTO Tasks (Name, Details, SID, DDate) VALUES ('{con}', '{det}', {opt}, '{date}')"
-        print(query)
         try:
             raw(query)
+            tk = raww("SELECT MAX(TID) FROM Tasks")
+            tk = tk[0][0]
+            for i in request.form:
+                if 'tag' in i:
+                    tq = f"INSERT INTO TT (TkID, TgID) VALUES ({tk}, {request.form[i]})"
+                    raw(tq)
             return redirect('/')
         except:
             return render_template('error.html', s="invalid input")
     else:
         setup()
         init()
-        uncom = raww("SELECT * FROM Tasks WHERE SID <> 2")
-        com = raww("SELECT * FROM Tasks WHERE SID = 2")
+        uncom = raww("SELECT * FROM Tasks WHERE SID <> 3")
+        com = raww("SELECT * FROM Tasks WHERE SID = 3")
         tags = raww("SELECT * FROM Tags")
         status = raww("SELECT * FROM Status")
-        return render_template('index.html', uncom = uncom, com = com, url=get_girl(), tags=tags, status=status)
+        tt = raww("SELECT * FROM TT")
+        with open("reset.txt", 'r') as f:
+            last = f.read()
+        return render_template('index.html', uncom = uncom, com = com, url=get_girl(), tags=tags, status=status, last=last, tt=tt)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -52,19 +64,30 @@ def delete(id):
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     if request.method == 'POST':
-        b = 1 if 'status' in request.form else 0
-        query = f"UPDATE Tasks SET Name = '{request.form['content']}', Status = {b} WHERE TID = {id}"
+        con = request.form['con']
+        det = request.form['det']
+        opt = request.form['opt']
+        date = request.form['date']
+        query = f"UPDATE Tasks SET Name = '{con}', Details = '{det}', SID = {opt}, DDate = '{date}' WHERE TID = {id}"
         try:
             raw(query)
+            raw(f"DELETE FROM TT WHERE TkID = {id}")
+            for i in request.form:
+                if 'tag' in i:
+                    tq = f"INSERT INTO TT (TkID, TgID) VALUES ({id}, {request.form[i]})"
+                    raw(tq)
             return redirect('/')
         except:
             return "There was an error"
     else:
         item = raww(f"SELECT * FROM Tasks WHERE TID = {id}")
+        status = raww("SELECT * FROM Status")
+        item = item[0]
+        tags = raww("SELECT * FROM Tags")
+        tt = raww("SELECT * FROM TT")
         if len(item) == 0:
             return render_template('error.html', s="Task ID invalid")
-        item = item[0]
-        return render_template('update.html', item=item)
+        return render_template('update.html', item=item, status=status, tags=tags,tt=tt)
 
 @app.errorhandler(HTTPException)
 def handleError(err):
